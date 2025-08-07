0be9b64588f8a8df752049898dfcb1665e105d0d
Karan-Kumar-Mishra <emiltokaranmishra@gmail.com>
2025-08-07 16:56:04 +0530

    add the one to one and one to many message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css';
 import { useEffect, useMemo, useRef, useState } from 'react';
 import { io } from 'socket.io-client';
-import { compile } from 'tailwindcss';
+
 
 function App() {
   const [socketId, setSocketId] = useState<string>('');
   const [messge, setmessage] = useState<string[]>([]);
-  const [room_id, setroom_id] = useState<string[]>([]);
   const roomRef = useRef<HTMLInputElement>(null);
   const socket = useMemo(() => io('http://localhost'), []);
 
@@ -16,6 +15,9 @@ function App() {
       console.log('Connected to the server');
       setSocketId(socket.id || "not found !");
       socket.on('new_msg', (data: string) => {
+        setmessage((prev) => [...prev, data]);
+      })
+      socket.on('grp_ntfy', (data: string) => {
         setmessage((prev) => [...prev, data]);
       })
     });
@@ -31,26 +33,30 @@ function App() {
   function handleKeyPress(e: React.KeyboardEvent<HTMLInputElement>) {
     if (e.key === 'Enter') {
       const msg = e.currentTarget.value;
-      console.log('Message:', msg);
-      console.log("room=>", roomRef.current?.value)
       const roomid = roomRef.current?.value.trim();
-      socket.emit('user_msg', { message: msg, room: roomid });
+      if (roomid == null || roomid.length == 0 || roomid == undefined) {
+        console.log("transmit the group message ....", msg)
+        socket.emit('grp_msg', msg);
+      }
+      else {
+        socket.emit('user_msg', { message: msg, room: roomid });
+      }
       e.currentTarget.value = '';
     }
   }
 
   return (
-    <div className="bg-neutral-800 h-screen  text-white flex justify-center items-center">
-      <div>
+    <div className="bg-neutral-800 h-screen  text-white flex justify-center flex-col items-center ">
+      <div className='font-extrabold font-serif m-2  w-200 h-100'>
         {messge.map((msg) => {
           return (
-            <h4>{msg}</h4>
+            <h4 key={msg}>{msg}</h4>
           )
         })}
       </div>
-      <div className="bg-neutral-400 mt-[20rem] gap-2 p-4 flex flex-col">
+      <div className="bg-neutral-400  gap-2 p-4 flex flex-col  w-200 rounded-s-sm">
         <input
-         className='border-2 border-black text-black p-2'
+          className='border-2 border-black text-black p-2'
           ref={roomRef}
           type="text"
           placeholder="Room Id"
@@ -58,7 +64,7 @@ function App() {
         <input
           type="text"
           onKeyDown={handleKeyPress}
-          className="w-80 p-2 text-black"
+          className=" p-2 text-black"
           placeholder="Type a message..."
         />
         <h4 className="bg-slate-600 mt-1 p-2">ID: {socketId}</h4>
